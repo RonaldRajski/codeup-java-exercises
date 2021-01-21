5b8f6b5736af0de761765e3535fc69e296c44f52
Ronald Rajski <ronaldrajski@gmail.com>
2021-01-21 14:44:55 -0600

    completed exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,87 @@
+
+
+
 public class HelloWorld {
-    public static void main(String[]args){
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+        int myFavoriteNumber = 7;
+        System.out.println("\nMy favorite number is "+myFavoriteNumber +"!");
+
+        String myString = "\nGo Cubs Go!";
+        System.out.println(myString);
+
+//        String myString1 = Y;
+//        System.out.println(myString1);
+
+        String myString2 = "3.14159";
+        System.out.println(myString2);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        double myNumber2 = 123L;
+        System.out.println(myNumber2);
+
+        long myNumber3 = 123;
+        System.out.println(myNumber3);
+
+//        float myNumber4 = 3.14;
+//        System.out.println(myNumber4);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        Displays 5 and then 6
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        displays 6 and 6
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////        int three = (int) o;
+//        Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
+//        at HelloWorld.main(HelloWorld.java:45)
+
+//        int three = (int) "three";
+//        Incompatible types error
+
+//        int x = 4;
+//        x +=5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *=  x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/MyFavoriteNumber.java
@@ -0,0 +1,7 @@
+public class MyFavoriteNumber {
+    public static void main(String[] args){
+    int myFavoriteNumber = 7;
+        System.out.print("My favorite number is "+myFavoriteNumber +"!");
+}
+}
+
