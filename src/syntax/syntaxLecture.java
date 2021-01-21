b69a49c9470acc7e4b90ea86b8b5c94f68697752
Ronald Rajski <ronaldrajski@gmail.com>
2021-01-21 11:15:49 -0600

    strings

--- src/syntax/syntaxLecture.java
+++ src/syntax/syntaxLecture.java
@@ -2,18 +2,54 @@ package syntax;
 
 public class SyntaxLecture {
 
+
+//    psvm is the shortcut
     public static void main(String[] args) {
+
+
 
         // ================== STATEMENTS (s-3)
 
         // Statements may be...
         // Declarations
 
+        int number;
+//        = is just assignment in Java
+//        == is assignment/value
+//        === is assignment/value/type
+
+
         // Assignment expressions
+        number = 7;
+
 
         // Any use of ++ or --
 
+//
+//        pre-decrement
+        --number;
+        System.out.println("number's value after pre-decrementing is:" + number);
+//        post-increment
+        number++;
+        System.out.println("number's value after post-decrementing is:" + number);
+
+
+       System.out.println("pre-decrementing " + number + " = " + ++number);
+
+        System.out.println("post-incrementing " + number + " = " + number++);
+        System.out.println("Now the number =" + number);
+
+
         // Method invocations
+        Math.random();
+
+//        assign random number to variable
+
+//        number = Math.random();
+        double randomNumber = Math.random();
+
+        System.out.println(randomNumber);
+
 
         // Object creation expressions
 
@@ -27,24 +63,67 @@ public class SyntaxLecture {
 
         // primitive types...
 
-//            byte age = -32;
+            byte age = 125;
+        System.out.println("Adding one to the Byte(125) : " + age++);
+        System.out.println("Adding one to the Byte(127) : " + age++);
+//        Loops back around to -128
+
 //        System.out.println(age);
-//            short minimumAnnualSalary = 15080;
-//            int distanceToSaturn = 1200000000;
-//            long numberOfHumanCells = 3000000000L; // L needed for compiler
-//            System.out.println(numberOfHumanCells);
-//            float lengthOfBacteriaInInches = 0.00004F; // F needed for compiler
-//            System.out.println(lengthOfBacteriaInInches);
-//            double widthOfAtomInMeters = .00000000001;
-//            char seatingSection = 'M'; // must wrap in single quotes
-//            boolean everythingIsAwesome = false;
+            short minimumAnnualSalary = 15080;
+        System.out.println("Adding one to the salary: " + ++minimumAnnualSalary);
+
+
+            int distanceToSaturn = 1200000000;
+
+
+            long numberOfHumanCells = 3000000000L; // L needed for compiler
+            System.out.println(numberOfHumanCells);
+            float lengthOfBacteriaInInches = 0.00004F; // F needed for compiler
+            System.out.println(lengthOfBacteriaInInches);
+            double widthOfAtomInMeters = .00000000001;
+            char seatingSection = 'M'; // must wrap in single quotes
+            boolean everythingIsAwesome = false;
+            if(false){
+                System.out.println("Everything is Awesome");
+            }
+            if(!everythingIsAwesome){
+                System.out.println("Everything is NOT Awesome");
+            }
 
         // strings (not a primitive type)
 
-//                 String thisIsAString = "Hello";
+                 String thisIsAString = "Hello";
+                    String anotherString = "Hello";
+
+                    if(thisIsAString == anotherString){
+                        System.out.println("The two are equal, from double equals");
+                    }
+
+        if (thisIsAString.equals(anotherString)) {
+            System.out.println("We used .equals, they are equivalent");
+
+        }
+
+        if(thisIsAString == "Hello"){
+            System.out.println("We used .equals, they are equivalent");
+
+        }
+
 
 
 //              like in JS, strings must escape certain characters like quotes and use it to inject newline characters and other formatting
+
+        System.out.println("hello\nWorld");
+        System.out.println("Hello");
+        System.out.println("World");
+
+        System.out.println("Hello, \"groovy\"World");
+
+//        char someLetter = "Y";
+//        System.out.println(someLetter);
+
+        String myName = "Casey";
+        System.out.println(myName.charAt(0));
 
 
         // ================== VARIABLES (s-8)
