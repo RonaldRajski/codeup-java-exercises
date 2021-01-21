5f77828bd8d35f9b70c59456c21d18f02a2c3b4c
Ronald Rajski <ronaldrajski@gmail.com>
2021-01-21 15:54:50 -0600

    imported scanner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 
 public class HelloWorld {
@@ -6,28 +6,48 @@ public class HelloWorld {
         System.out.print("Hello, ");
         System.out.print("World!");
 
+//        Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+
         int myFavoriteNumber = 7;
         System.out.println("\nMy favorite number is "+myFavoriteNumber +"!");
+
+//        Create a String variable named myString and assign a string value to it, then print the variable out to the console.
 
         String myString = "\nGo Cubs Go!";
         System.out.println(myString);
 
-//        String myString1 = Y;
-//        System.out.println(myString1);
+//        Change your code to assign a character value to myString. What do you notice?
+
+        String myString1 = "Y";
+        System.out.println(myString1);
+
+//        Change your code to assign the value 3.14159 to myString. What happens?
 
         String myString2 = "3.14159";
         System.out.println(myString2);
 
-        double myNumber = 3.14;
-        System.out.println(myNumber);
+//        Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+//
+//        long myNumber;
+//        System.out.println(myNumber);
+
+
+//        double myNumber1 = 3.14;
+//        System.out.println(myNumber1);
+
+//        Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
 
         double myNumber2 = 123L;
         System.out.println(myNumber2);
 
-        long myNumber3 = 123;
-        System.out.println(myNumber3);
+//        Change your code to assign the value 123 to myNumber.
+//
+//        Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
 
-//        float myNumber4 = 3.14;
+//        long myNumber3 = 123;
+//        System.out.println(myNumber3);
+//
+//        float myNumber4 = 3.14F;
 //        System.out.println(myNumber4);
 
 //        int x = 5;
@@ -42,7 +62,7 @@ public class HelloWorld {
 
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
-////        int three = (int) o;
+//        int three = (Integer) o;
 //        Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
 //        at HelloWorld.main(HelloWorld.java:45)
 
@@ -66,9 +86,8 @@ public class HelloWorld {
         System.out.println(y);
 
 
-
-
-
+        int bigNumber = Integer.MAX_VALUE;
+        System.out.println(++bigNumber);
 
 
 
