5e2310aafe8dce9778b808a4e1964bc4e1612d99
Ronald Rajski <ronaldrajski@gmail.com>
2021-01-25 23:22:14 -0600

    completed control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
 //        int i = 5;
 //        while (i <= 15) {
-//            System.out.println(i);
+//            System.out.print(i);
+//            System.out.print(' ');
 //            i++;
-//        };
+//        }
+
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.printf("%d ", i);
+//        }
 
 
         //        Do While Loop
 
-//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+
 //        Alter your loop to count backwards by 5's from 100 to -10.
 //        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
 
@@ -24,18 +29,48 @@ public class ControlFlowExercises {
 //        256
 //        65536
 
+//            for(int j = 2; j <=100; j+=2){
+//                System.out.println(j);
+//            }
 
-//        int i =100;
-
-
-//        do{
-//            System.out.println("i is " + i);
-//            i+=2;
-//            i-=5;
+//        int j = 0;
+//        do {
+//            j += 2;
+//            System.out.println(j);
 //        }
-//        while(i<=100);
-//        while(i>=0);
-//        while(i < 1000000);
+
+//            for(int k = 100; k>=-10; k-=5){
+//                System.out.println(k);
+//            }
+//        while (j < 100);
+
+//        int k = 105;
+//        do {
+//            k-=5;
+//            System.out.println(k);
+//        }
+//        while(k > -10);
+
+        //        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        for (long m =2; m <1000000; m*=m){
+//            System.out.println(m);
+//        }
+
+//        long m = 2;
+//        do{
+//            System.out.println(m);
+//            m *= m;
+//
+//        }
+//        while(m < 1000000);
+
+
+
+
+
+
+
+
 
 
 //        2. Fizzbuzz
@@ -51,28 +86,38 @@ public class ControlFlowExercises {
 //                System.out.println(i);
 //        }
 
-        int i;
-        String userInput;
-        int baseInt;
-        int maxInt;
-        do {
-            System.out.println("Enter an integer.");
-            baseInt = scan.nextInt();
+//        int i;
+//        String userInput;
+//        int baseInt;
+//        int maxInt;
+//        do {
+//            System.out.println("Enter an integer.");
+//            baseInt = scan.nextInt();
             System.out.println("What number would you like to go up to?");
-            maxInt = scan.nextInt();
+            int input = scanner.nextInt();
+        System.out.println("\nHere is the number you entered!\n ");
             System.out.print("number | squared | cubed\n" +
-                    "---- | ----- | ---\n");
-            for (i = baseInt; i <= maxInt; ++i){
-                System.out.print(i + "\t| " + (i * i) + "\t| " + (i * i * i) + "\n");
+                    "------ | ------- | ---\n");
+            for (int p = 1; p <= input; p++){
+                System.out.printf("%-6d | %-7d | %-5d%n", p, p*p, p*p*p);
+//                System.out.print(p + " ");
+//                System.out.print(p*p + " ");
+//                System.out.println(p*p*p);
+
+//              System.out.print(i + "\t| " + (i * i) + "\t| " + (i * i * i) + "\n");
         }
-            System.out.println("\nWould you like to continue? [yes/no]");
-            userInput = scan.next();
-            ++i;
-        } while (userInput.equals("yes"));
+//            System.out.println("\nWould you like to continue? [yes/no]");
+//            userInput = scan.next();
+//            ++i;
+//        } while (userInput.equals("yes"));
+
+
+  }
+          }
 
 
 
 
 
-    }
-}
+
+
