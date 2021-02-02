0c141e0d5b25b3419182829c8fb953c1f20a2cbc
Ronald Rajski <ronaldrajski@gmail.com>
2021-02-02 10:12:45 -0600

    downloaded MoviesArray

--- src/Movies/Movie.java
+++ src/Movies/Movie.java
@@ -1,4 +1,39 @@
 package Movies;
 
+import util.Input;
+
+
+
 public class Movie {
+//    Create a class named Movie. X
+//    private fields for name and category,
+
+    private String name;
+    private String category;
+
+    public Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+
+    //    Create methods to access these properties and change them (getters and setters).
+    public String getMovie(){
+        return this.name + " " + this.category;
+    }
+
+    public String setName(String name){
+        name = this.name;
+        return name;
+    }
+
+    public String setCategory(String category){
+        category = this.category;
+        return category;
+    }
+
+    public String setMovie(String name, String Category){
+        name = this.name;
+        category = this.category;
+        return name + " " + category;
+    }
 }
