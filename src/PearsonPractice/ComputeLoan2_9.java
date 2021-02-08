6976c7fb3a8d86cde39f67f7e778e92863557e99
Ronald Rajski <ronaldrajski@gmail.com>
2021-02-07 18:25:46 -0600

    chapter 2

---
+++ src/PearsonPractice/ComputeLoan2_9.java
@@ -0,0 +1,42 @@
+package PearsonPractice;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class ComputeLoan2_9 {
+    public static void main(String[] args) {
+//        Create a scanner
+        Scanner input =  new Scanner(System.in);
+
+//        Enter Yearly interest rate
+        System.out.println("Enter annual interest rate:");
+        double annualInterestRate = input.nextDouble();
+
+//        obtain monthly interest rate
+        double monthlyInterestRate = annualInterestRate/1200;
+
+//        Enter number of years
+        System.out.println("Enter number of years as an integer:  ");
+        int numberOfYears = input.nextInt();
+
+//        enter loan amount
+        System.out.println("Enter loan amount: ");
+        double loanAmount = input.nextDouble();
+
+//        calculate payment
+        double monthlyPayment = loanAmount *monthlyInterestRate/ (1- 1/Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
+        double totalPayment =  monthlyPayment + numberOfYears * 12;
+
+
+        System.out.println("The monthly payment is $ " + (int)(monthlyPayment * 100)/100.0);
+
+        System.out.println("The total payment is $ " + (int)(totalPayment * 100)/100.0);
+
+
+
+
+    }
+
+
+}
---
+++ src/PearsonPractice/SalesTax2_8.java
@@ -0,0 +1,32 @@
+package PearsonPractice;
+
+import java.util.Scanner;
+
+public class SalesTax2_8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter purchase amount: ");
+       double purchaseAmount = input.nextDouble();
+
+
+       double tax =  purchaseAmount * 0.06;
+        System.out.println("Sales tax is " + (int)(tax * 100)/100);
+
+        double amount = 5;
+        System.out.println(amount/2);
+        System.out.println(5/2);
+
+
+        System.out.println((int) (45.378 * 100) / 100);
+        System.out.println((int) (45.378 * 100) / 100.0);
+        System.out.println((int) (45.378 * 100 / 100));
+        System.out.println((int) (45.378) * 100 / 100.0);
+
+        System.out.println((int) (76.0252175 * 100) / 100);
+
+
+
+
+    }
+}
