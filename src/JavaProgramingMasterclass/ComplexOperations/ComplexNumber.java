726d62e1481a1798f18fbd6318a555a00db38c2a
Ronald Rajski <ronaldrajski@gmail.com>
2021-02-10 10:07:29 -0600

    completed complex number

--- src/JavaProgramingMasterclass/ComplexOperations/ComplexNumber.java
+++ src/JavaProgramingMasterclass/ComplexOperations/ComplexNumber.java
@@ -18,8 +18,27 @@ public class ComplexNumber {
         return imaginary;
     }
 
+    public void add(double real, double imaginary){
+        this.real = this.real + real;
+        this.imaginary = this.imaginary + imaginary;
+    }
+
     public void add(ComplexNumber complexNumber){
         this.real = this.real + complexNumber.getReal();
         this.imaginary = this.imaginary + complexNumber.getImaginary();
     }
+
+    public void subtract(ComplexNumber complexNumber){
+        this.real = this.real - complexNumber.getReal();
+        this.imaginary = this.imaginary - complexNumber.getImaginary();
+    }
+
+
+    public void subtract(double real, double imaginary){
+        this.real = this.real - real;
+        this.imaginary = this.imaginary - imaginary;
+    }
+
+
+
 }
