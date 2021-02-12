f03333a18671cc361def2148a2c0ce5719d260d6
Ronald Rajski <ronaldrajski@gmail.com>
2021-02-11 21:27:59 -0600

    Programming assignment 2

---
+++ src/PearsonPractice/SimpleIfDemo3_2.java
@@ -0,0 +1,20 @@
+package PearsonPractice;
+import java.util.Scanner;
+
+public class SimpleIfDemo3_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int number = input.nextInt();
+
+        if(number % 5 == 0){
+            System.out.println("HiFive");
+        }
+
+        if(number % 2 == 0){
+            System.out.println("HiEven");
+        }
+
+    }
+
+}
