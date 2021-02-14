1a0ba190fa4869425e095efb0387248039e4af65
Ronald Rajski <ronaldrajski@gmail.com>
2021-02-13 19:54:57 -0600

    leap Year

--- src/PearsonPractice/Chapter3/TestBooleanOperators3_6.java
+++ src/PearsonPractice/Chapter3/TestBooleanOperators3_6.java
@@ -1,4 +1,30 @@
 package PearsonPractice.Chapter3;
 
+import java.util.Scanner;
+
 public class TestBooleanOperators3_6 {
+
+    public static void main(String[] args) {
+//        Create Scanner
+
+        Scanner input = new Scanner(System.in);
+
+//        Receive input
+        System.out.println("Enter an integer: ");
+        int number = input.nextInt();
+
+        if(number % 2 == 0 && number % 3 == 0){
+            System.out.println(number + " is divisible  by 2 & 3");
+        }
+        if(number % 2 == 0 || number % 3 == 0){
+            System.out.println(number + " is divisible by 2 or 3.");
+        }
+        if(number % 2 == 0 ^ number % 3 == 0){
+            System.out.println(number + " divisible by 2 or 3, but not by both");
+        }
+
+
+
+    }
+
 }
