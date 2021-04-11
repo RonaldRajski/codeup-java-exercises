63efb11f3d4125dae09aa658ec72fea643de32a3
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-10 21:32:37 -0500

    defining a method practice

--- src/Practice4/Chapter6methods/MethodDemo.java
+++ src/Practice4/Chapter6methods/MethodDemo.java
@@ -15,6 +15,48 @@ public class MethodDemo {
         System.out.println("Sum from 20 to 37 is " + sum(20,37));
         System.out.println("Sum from 35 to 39 is " + sum(35,49));
     }
+//
+//    Write the definition of a method twice, which receives an int parameter and returns an int that is twice the value of the parameter.
+
+    public static int twice(int x){
+        return x*2;
+    }
+
+//    Write the definition of a method add, which receives two int parameters and returns their sum.
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+//
+//    Write the definition of a method powerTo, which receives two parameters. The first is a double and the second is an int. The method returns a double.
+//
+//    If the second parameter is negative, the method returns zero. Otherwise it returns the value of the first parameter raised to the power of the second parameter.
+
+    public double powerTo(double a, int b){
+        if(a < 0){
+            return 0.0;
+        }
+        else {
+            return Math.pow(a, (double) b);
+        }
+    }
+
+//    Write the definition of a method min that has two int parameters and returns the smaller.
+
+    public int min (int a, int b){
+        return Math.min(a,b);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
