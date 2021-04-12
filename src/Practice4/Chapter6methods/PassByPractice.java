f43dfbcaedb68381a68e76fff4edb58da58e2dba
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-11 23:57:30 -0500

    practice 4

--- src/Practice4/Chapter6methods/PassByPractice.java
+++ src/Practice4/Chapter6methods/PassByPractice.java
@@ -19,19 +19,19 @@ public class PassByPractice {
 
 //        b
 
-        int i = 1;
-        while (i<=6){
-            method1(i,2);
-            i++;
-        }
-    }
-
-    public static void method1(int i, int num) {
-        for (int j = 1; j<=i; j++){
-            System.out.println(num + " ");
-            num *=2;
-        }
-        System.out.println();
+//        int i = 1;
+//        while (i<=6){
+//            method1(i,2);
+//            i++;
+//        }
+//    }
+//
+//    public static void method1(int i, int num) {
+//        for (int j = 1; j<=i; j++){
+//            System.out.println(num + " ");
+//            num *=2;
+//        }
+//        System.out.println();
 
 //        (b)
 //                2
@@ -40,7 +40,47 @@ public class PassByPractice {
 //        2 4 8 16
 //        2 4 8 16 32
 //        2 4 8 16 32 64
+//    }
+//}
+//        c
+//        Initialize times
+//        int times = 3;
+//        System.out.println("Before the call, " + " variable times is " + times);
+//    }
+////    Print the message n times
+//
+//    public static void nPrintln(
+//            String message, int n){
+//        while (n>0){
+//            System.out.println("n= " + n);
+//            System.out.println(message);
+//            n--;
+//        }
+//
+//    }
+
+        int i = 0;
+        while (i <= 4) {
+            method1(i);
+            i++;
+        }
+
+        System.out.println("i is " + i);
+    }
+
+    public static void method1(int i) {
+        do {
+            if (i % 3 != 0)
+                System.out.print(i + " ");
+            i--;
+        }
+        while (i >= 1);
+
+        System.out.println();
+
+
     }
 }
+
 
 
