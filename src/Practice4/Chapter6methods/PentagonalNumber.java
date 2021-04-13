bfbc02224119a3d4796bc8185b3a7ea56638ed25
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-13 08:11:11 -0500

    more array practice

---
+++ src/Practice4/Chapter6methods/PentagonalNumber.java
@@ -0,0 +1,12 @@
+package Practice4.Chapter6methods;
+
+public class PentagonalNumber {
+//    public static void main(String[] args) {
+//
+//
+//
+//    }
+//    public static int getPentagonalNumber(int n){
+//
+//    }
+}
---
+++ src/Practice4/Chapter7Arrays/Chapter7Arrays.java
@@ -0,0 +1,39 @@
+package Practice4.Chapter7Arrays;
+
+public class Chapter7Arrays {
+
+    public static void main(String[] args) {
+        //    Declare and instantiate an array named scores of 25 elements of type int.
+
+        int scores[] = new int[25];
+
+//        Write a statement that declares an array named streetAddress that contains exactly 80 elements of type char.
+
+        char streetAddress[] = new char[80];
+
+        int[] x = new int[3];
+        System.out.println("x[0] is " + x[0]);
+
+//        Assume that an array of integers named salarySteps that contains exactly 5 elements has been declared.
+//
+//        Write a statement that assigns the value 160000 to the last element of the array salarySteps
+
+        salarySteps[4]=160000;
+        salarySteps[salarySteps.length - 1] = 160000;
+
+//        Assume that an array of ints named a has been declared with 12 elements. The integer variable k holds a value between 0 and 6. Assign 15 to the array element whose index is k.
+        a[k]=15;
+//
+//        An array of ints named a has been declared with 12 elements. The integer variable k holds a value between 0 and 6. Assign 9 to the element just after a[k].
+
+        a[k+1] = 9;
+
+
+    }
+}
+
+
+
+
+
+
