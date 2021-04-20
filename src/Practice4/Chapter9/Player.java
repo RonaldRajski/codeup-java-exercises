f4a64c5e36a25758fd36609d04faf620cf36f5b8
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-19 23:43:52 -0500

    Player class getters and setters

---
+++ src/Practice4/Chapter9/Player.java
@@ -0,0 +1,22 @@
+package Practice4.Chapter9;
+
+public class Player {
+     private String name = "";
+     private int score = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
