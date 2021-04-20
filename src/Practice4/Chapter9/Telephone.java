c5e9b20854d13ebb797a1f58f1167fad0f16ebe4
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-19 23:55:51 -0500

    public class telephone

---
+++ src/Practice4/Chapter9/Telephone.java
@@ -0,0 +1,9 @@
+package Practice4.Chapter9;
+
+//Write the definition of a class Telephone. The class has no constructors and one static method printNumber. The method accepts a String argument and prints it on the screen. The method returns nothing.
+
+public class Telephone {
+    public static void printNumber(String s) {
+        System.out.print(s);
+    }
+}
