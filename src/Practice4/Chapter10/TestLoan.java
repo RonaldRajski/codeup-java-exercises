c59260ecb3b47140945bbbde3c588ab4e867ea0a
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-23 04:27:58 -0500

    completed loan test

--- src/Practice4/Chapter10/TestLoan.java
+++ src/Practice4/Chapter10/TestLoan.java
@@ -9,8 +9,26 @@ public class TestLoan {
 //        scanner
         Scanner input = new Scanner(System.in);
 
-//        Enter yearly interrst rate
-        System.out.println("Enter annual interest rate, for e");
+//        Enter yearly interest rate
+        System.out.println("Enter annual interest rate, for example, 8.25");
+        double annualInterestRate =  input.nextDouble();
+
+//        Enter number of years
+        System.out.println("Enter number of years as an interger: ");
+        int numberOfYears =  input.nextInt();
+
+//        Enter loan amount
+        System.out.println("Enter loan amount, for example, 120000.95:" );
+        double loanAmount = input.nextDouble();
+
+//        Create Loan object
+        Loan loan = new Loan(annualInterestRate, numberOfYears, loanAmount);
+
+//        display loan date, monthly payment and total payment
+        System.out.printf("The loan was created on %s\n", loan.getLoanDate().toString(), loan.getMonthlyPayment(), loan.getTotalPayment());
+
+
+
 
 
     }
