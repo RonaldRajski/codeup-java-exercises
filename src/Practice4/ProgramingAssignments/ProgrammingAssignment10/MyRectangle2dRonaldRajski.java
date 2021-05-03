861a0a1f740432ade5d52c9aaca9244abf858582
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-02 20:41:56 -0500

    my 2d rectangleTest

--- src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dRonaldRajski.java
+++ src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dRonaldRajski.java
@@ -5,32 +5,52 @@
 //        Due:  5/2/2021
 //<brief description of the problem that the program was written to solve>
 
-Define the MyRectangle2D class that contains:
-        •	Two double data fields named x and y that specify the center of the rectangle, with getter and setter methods. (Assume the rectangle sides are parallel to the x- or y- axis).
-        •	Two double data fields named width and height that specify the width and height of the rectangle, with getter and setter methods.
-        •	A no-arg constructor that creates a default rectangle with (0,0) for (x,y) and 1 for both width and height.
-        •	A constructor that creates a rectangle with the specified x, y, width and height.
-        •	A method named getArea() that returns the area of this rectangle.
-        •	A method named getPerimeter() that returns the perimeter of the rectangle.
-        •	A method contains(double x, double y) that returns true if the specified point (x,y) is inside this rectangle
-        •	A method contains(MyRectangle2D r) that returns true if the specified rectangle is inside this rectangle.
-        •	A method overlaps(MyRectangle2D r) that returns true if the specified rectangle overlaps this rectangle.
-        Draw the UML diagram for the class and then implement the class.
-        Write a test program that creates a MyRetangle2D object r1 (new MyRectangle2D(2, 2, 5.5, 4.9)) , displays its area and perimeter, and displays the result of r1.contains(3,3), r1.contains(new MyRectangle2D(4, 5,10.5, 3.2)) and r1.overlaps (new MyRectangle2D(3, 5, 2.3, 5.4)).
-        Your program’s output should look like the sample shown below:
-        rectangle r1 is centered at (2.0,2.0) with width 5.5 and height 4.9
-        Area is 26.950000000000003
-        Perimeter is 20.8
-        r1 contains (3,3) is true
-        r1.contains(new MyRectangle2D(4, 5, 10.5, 3.2)) is false
-        r1.overlaps(new MyRectangle2D(3, 5, 2.3, 5.4)) is true
+//Define the MyRectangle2D class that contains:
+//        •	Two double data fields named x and y that specify the center of the rectangle, with getter and setter methods. (Assume the rectangle sides are parallel to the x- or y- axis).
+//        •	Two double data fields named width and height that specify the width and height of the rectangle, with getter and setter methods.
+//        •	A no-arg constructor that creates a default rectangle with (0,0) for (x,y) and 1 for both width and height.
+//        •	A constructor that creates a rectangle with the specified x, y, width and height.
+//        •	A method named getArea() that returns the area of this rectangle.
+//        •	A method named getPerimeter() that returns the perimeter of the rectangle.
+//        •	A method contains(double x, double y) that returns true if the specified point (x,y) is inside this rectangle
+//        •	A method contains(MyRectangle2D r) that returns true if the specified rectangle is inside this rectangle.
+//        •	A method overlaps(MyRectangle2D r) that returns true if the specified rectangle overlaps this rectangle.
+//        Draw the UML diagram for the class and then implement the class.
+//        Write a test program that creates a MyRetangle2D object r1 (new MyRectangle2D(2, 2, 5.5, 4.9)) , displays its area and perimeter, and displays the result of r1.contains(3,3), r1.contains(new MyRectangle2D(4, 5,10.5, 3.2)) and r1.overlaps (new MyRectangle2D(3, 5, 2.3, 5.4)).
+//        Your program’s output should look like the sample shown below:
+//        rectangle r1 is centered at (2.0,2.0) with width 5.5 and height 4.9
+//        Area is 26.950000000000003
+//        Perimeter is 20.8
+//        r1 contains (3,3) is true
+//        r1.contains(new MyRectangle2D(4, 5, 10.5, 3.2)) is false
+//        r1.overlaps(new MyRectangle2D(3, 5, 2.3, 5.4)) is true
 
 
+/**********************************************************************
+ *                          MyRectangle2D                              *
+ *---------------------------------------------------------------------*
+ * -x: double                                                          *
+ * -y: double                                                          *
+ * -width: double                                                      *
+ * -height: double                                                     *
+ * +MyRectangle2DRonaldRajski()                                                    *
+ * +MyRectangle2DRonaldRajski(x: double, y: double, width: double, height: double) *
+ * +setX(x: double )                                                   *
+ * +setY(y: double)                                                    *
+ * +getX(): double                                                     *
+ * +getY(): double                                                     *
+ * +setHeight(heigth: double)                                          *
+ * +setWidth(width: double)                                            *
+ * +getHeight(): double                                                *
+ * +getWidth(): double                                                 *
+ * +getArea(): double                                                  *
+ * +getPermeter(): double                                              *
+ * +contains(r: MyRectangle2D)                                         *
+ * +contains(x: double, y: double)                                     *
+ * +overlaps(r: MyRectangle2D)                                         *
+ **********************************************************************/
 
-
-
-
-        package Practice4.ProgramingAssignments.ProgrammingAssignment10;
+package Practice4.ProgramingAssignments.ProgrammingAssignment10;
 
 public class MyRectangle2dRonaldRajski {
 }
--- src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dTestRonaldRajski.java
+++ src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dTestRonaldRajski.java
@@ -1,4 +1,20 @@
 package Practice4.ProgramingAssignments.ProgrammingAssignment10;
 
 public class MyRectangle2dTestRonaldRajski {
+//    Main Method
+public static void main(String[] args) {
+//    Create a myRectangle2D object
+    MyRectangle2dRonaldRajski r1 = new MyRectangle2dRonaldRajski(2,2,5.5,4.9);
+
+    System.out.println("\nRectangle");
+    System.out.println("Area: " + r1.getArea());
+    System.out.println("Perimeter: " + r1.getPerimeter());
+    System.out.println(r1.contains(3,3) ? "Contains" : "Does not contain") + " the point (3,3).";
+    System.out.println(r1.contains(new MyRectangle2dRonaldRajski(4,5,10.5,3.2)) ? "Contains" : "Does Not contain") + "  the rectangle with points (4, 5, 10.5, 3.2)");
+    System.out.println(r1.overlaps(new MyRectangle2dRonaldRajski(3,5,2.3,5.4)) ? "Overlaps" : "Does not overlap" + "the rectangle with points (3,5,2.3,5.4)");
+
 }
+
+
+}
+
