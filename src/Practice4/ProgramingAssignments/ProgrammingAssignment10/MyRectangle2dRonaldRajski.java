e9cf3a6c171e3f5f38689e4f7736f3752805a46a
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-02 21:58:43 -0500

    completed assignment

--- src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dRonaldRajski.java
+++ src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dRonaldRajski.java
@@ -53,4 +53,98 @@
 package Practice4.ProgramingAssignments.ProgrammingAssignment10;
 
 public class MyRectangle2dRonaldRajski {
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    MyRectangle2dRonaldRajski(){
+        this(0,0,1,1);
+    }
+
+    public MyRectangle2dRonaldRajski(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea(){
+        return width * height;
+    }
+
+    public double getPerimeter(){
+        return 2 * (width + height);
+    }
+
+    /** Returns true if the specified point(x, y)
+     *   is inside the rectangle */
+    public boolean contains(double x, double y) {
+        return getDistance(this.y, y) <= height / 2 &&
+                getDistance(this.x, x) <= width / 2;
+    }
+
+    /** Returns true if the specified
+     *   rectangle is inside this rectangle */
+    public boolean contains(MyRectangle2dRonaldRajski r) {
+        return
+                getDistance(y, r.getY()) + r.getHeight() / 2 <= height / 2 &&
+                        getDistance(x, r.getX()) + r.getWidth() / 2 <= width / 2 &&
+                        height / 2 + r.getHeight() / 2 <= height &&
+                        width / 2 + r.getWidth() / 2 <= width;
+    }
+
+    /** Returns true if the specified rectangle
+     *   overlaps with this rectangle */
+    public boolean overlaps(MyRectangle2dRonaldRajski r) {
+        return !contains(r) &&
+                ((x + width / 2 > r.getX() - r.getWidth()) ||
+                        (y + height / 2 > r.getY() - r.getHeight())) &&
+                (getDistance(y, r.getY()) < height / 2 + r.getHeight() / 2) &&
+                (getDistance(x, r.getX()) < width / 2 + r.getWidth() / 2);
+    }
+
+    /** Return distance */
+    private double getDistance(double p1, double p2) {
+        return Math.sqrt(Math.pow(p2 - p1, 2));
+
+    }
+
 }
+
+
+
+
+
--- src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dTestRonaldRajski.java
+++ src/Practice4/ProgramingAssignments/ProgrammingAssignment10/MyRectangle2dTestRonaldRajski.java
@@ -6,12 +6,13 @@ public static void main(String[] args) {
 //    Create a myRectangle2D object
     MyRectangle2dRonaldRajski r1 = new MyRectangle2dRonaldRajski(2,2,5.5,4.9);
 
-    System.out.println("\nRectangle");
-    System.out.println("Area: " + r1.getArea());
-    System.out.println("Perimeter: " + r1.getPerimeter());
-    System.out.println(r1.contains(3,3) ? "Contains" : "Does not contain") + " the point (3,3).";
-    System.out.println(r1.contains(new MyRectangle2dRonaldRajski(4,5,10.5,3.2)) ? "Contains" : "Does Not contain") + "  the rectangle with points (4, 5, 10.5, 3.2)");
-    System.out.println(r1.overlaps(new MyRectangle2dRonaldRajski(3,5,2.3,5.4)) ? "Overlaps" : "Does not overlap" + "the rectangle with points (3,5,2.3,5.4)");
+
+    System.out.println("rectangle r1 is centered at (" +r1.getX() + "," +r1.getY() + ") with width " +r1.getWidth()+  " and height " + r1.getHeight());
+    System.out.println("Area is " + r1.getArea());
+    System.out.println("Perimeter is " + r1.getPerimeter());
+    System.out.println((r1.contains(3, 3) ? "r1 Contains" : " r1 Does not contain") + " the point (3, 3) is true");
+    System.out.println((r1.contains(new MyRectangle2dRonaldRajski(4, 5, 10.5, 3.2)) ? "r1.contains" : "r1:contains") + " (new MyRectangle2D(4, 5, 10.5, 3.2) is false");
+    System.out.println((r1.overlaps(new MyRectangle2dRonaldRajski(3, 5, 2.3, 5.4)) ? "r1.overlaps" : "r1:overlaps") + " (new MyRectangle2D(3, 5, 2.3, 5.4) is true");
 
 }
 
